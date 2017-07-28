416b71ee754faa8750d8a73c3ac2237225e31311
“Marion” <marionacp@gmail.com>
2017-07-28 13:50:46 -0400

    cargando ej

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.button-collapse').sideNav({
+		manuWidth:170,
+		edge:'left',
+		closeOnClick:true,
+		draggable:true,
+	});
+});
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
+
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js','node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	.pipe(concat('script.js'))
+	//caroeta dust
+	.pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('style', function(){
+	gulp.src(['node_modules/materialize-css/dist/css/materialize.css','assets/sass/main.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('webserver',function(){
+	gulp.src('../gulpie/')
+	.pipe(webserver({
+		fallback: 'index.html',
+		livereload: true,
+		directoryListing:false,
+		open:true,
+		port:8001
+	}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/*.scss',['style']);
+});
+
+gulp.task('default', ['script','style','webserver','watch']);
